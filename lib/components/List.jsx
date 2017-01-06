35596f44441ee9022343512366bf1ab088b67742
Adam Rice <adamcoltonrice@gmail.com>
2017-01-05 21:59:27 -0700

    rendorContacts method

--- lib/components/List.jsx
+++ lib/components/List.jsx
@@ -1,19 +1,53 @@
 /*jshint esversion: 6 */
+import React, { Component } from 'react';
+import {
+  Text,
+  TouchableHighlight,
+} from 'react-native';
 
-const List = ({ contacts, navigator }) => {
+export default class List extends Component {
+  navigate() {
+    this.props.navigator.push({
+      title: 'ContactCard'
+    });
+  }
+
+  render () {
+    const { contacts } = this.props;
+
+    const renderContacts = () {
+      return contacts.map((contact) => {
+        return (
+          <TouchableHighlight onPress={()=> this.navigate(contact.id)}>
+            <Text>{contact.name} | {contact.title} | {contact.org}</Text>
+          </TouchableHighlight>
+        );
+      });
+    }
+
+    return (
+      <Text>Contacts</Text>
+    )
+
+    );
+  }
+
+}
+
+
   const renderContacts = () {
     return contacts.map((contact) => {
       return (
-        <ContactCard key={contact.id} {...contact}/>
+
+        <TouchableHighlight onPress={()=> this.navigate(contact.id)}>
+          <Text>{list}</Text>
+        </TouchableHighlight>
+
+
+        <ContactCard navigator={navigator} key={contact.id} {...contact}/>
       );
     });
   }
 };
 
-// do we need this?
-//
-// navigate(){
-//   this.props.navigator.push({
-//     title: 'ContactCard',
-//   });
-// }
+export default List;
