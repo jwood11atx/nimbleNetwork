54beb85c8457b1f64511dfac60d5a826a9356a09
Adam Rice <adamcoltonrice@gmail.com>
2017-01-05 22:20:51 -0700

    wip Contacts component

--- lib/components/AddContact.jsx
+++ lib/components/AddContact.jsx
@@ -1,11 +1,22 @@
 /*jshint esversion: 6 */
-import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
   Text,
   View,
-  Dimensions,
   TouchableHighlight,
   TextInput,
 } from 'react-native';
+
+const AddContact = () => {
+  return (
+    <View>
+      <TextInput style={{height: 40, marginTop: 10, textAlign: 'center', backgroundColor: '#98cb00'}} placeholder='Enter a Name'/>
+      <TextInput style={{height: 40, marginTop: 10, textAlign: 'center', backgroundColor: '#98cb00'}} placeholder='Enter Title'/>
+      <TextInput style={{height: 40, marginTop: 10, textAlign: 'center', backgroundColor: '#98cb00'}} placeholder='Enter Organization'/>
+      <TouchableHighlight style={{marginTop: 20, alignItems: 'center'}}>
+        <Text>Add</Text>
+      </TouchableHighlight>
+    </View>
+  )
+};
+
+export default AddContact;
