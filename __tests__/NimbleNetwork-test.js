5accfbb5dbd249695cf24ade35b4c9ae818e94ef
Joshua Small <jksmall0631@gmail.com>
2017-01-09 16:25:58 -0700

    style contacts and download dependencies

---
+++ __tests__/NimbleNetwork-test.js
@@ -0,0 +1,17 @@
+import 'react-native';
+import React from 'react';
+import NimbleNetwork from '../lib/components/NimbleNetwork.js';
+import renderer from 'react-test-renderer';
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import Jest from 'jest';
+
+// Note: test renderer must be required after react-native.
+
+it('It renders', () => {
+  const tree = renderer.create(
+    <NimbleNetwork />
+  ).toJSON();
+  console.log(tree);
+  expect(tree).toMatchSnapshot();
+});
